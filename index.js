278380104c66d831a183cafc8eb5b78e4e23890e
Ahmedu22 <ahmedgithub2236@gmail.com>
2025-05-02 15:21:10 +0300

    java-assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+var name = prompt("what's your name");
+console.log(name);
+
+
+var age = prompt("what's your age");
+console.log(age);
+
+
+var color = prompt("whats your favorite color");
+console.log(color);
+
+var name = "Ahmed";
+console.log("name");
+console.log("25");
+console.log("blue")
+
+
+console.log(typeof "ahmed");
+console.log(typeof 25);
+console.log(typeof true)
+
+
